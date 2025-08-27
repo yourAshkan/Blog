658fe5e9aeab8e55df5ba03de8a7013fbd6dfb3a
yourAshkan <ashkan.kiya11@gmail.com>
2025-08-27 17:24:28 +0330

    TST - 3 - Create Null Constructor in Article.cs

--- Blog/Model/Entity/Article.cs
+++ Blog/Model/Entity/Article.cs
@@ -1,14 +1,31 @@
 ﻿namespace Blog.Model.Entity
 {
-    public class Article(string title,string picture, string pictureAlt, string pictureTitle,string shortDescription,string description)
+    public class Article
     {
         public int Id { get; set; }
-        public string? Title { get; private set; } = title;
-        public string? Picture { get; private set; } = picture;
-        public string? PictureALT { get; private set; } = pictureAlt;
-        public string? PictureTitle { get; private set; } = pictureTitle;
-        public string? ShortDescription { get; private set; } = shortDescription;
-        public string? Description { get; private set; } = description;
-        public DateTime? CreationDate { get; private set; } = DateTime.Now;
+        public string? Title { get; private set; }
+        public string? Picture { get; private set; }
+        public string? PictureALT { get; private set; }
+        public string? PictureTitle { get; private set; }
+        public string? ShortDescription { get; private set; }
+        public string? Description { get; private set; }
+        public DateTime? CreationDate { get; private set; }
+        public string? PictureTilte { get; }
+
+        public Article()
+        {
+
+        }
+
+        public Article(string? title, string? picture, string? pictureALT, string? pictureTilte, string? shortDescription,string? description)
+        {
+            Title = title;
+            Picture = picture;
+            PictureALT = pictureALT;
+            PictureTilte = pictureTilte;
+            ShortDescription = shortDescription;
+            Description = description;
+            CreationDate = DateTime.Now;
+        }
     }
 }

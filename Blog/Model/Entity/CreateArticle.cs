3fe20682f941dec1e3775a4ce7e9fc1fd149d7b4
yourAshkan <ashkan.kiya11@gmail.com>
2025-08-27 14:50:27 +0330

    TST - 1 - CreateArticle.cs in Model

---
+++ Blog/Model/Entity/CreateArticle.cs
@@ -0,0 +1,12 @@
+﻿namespace Blog.Model.Entity
+{
+    public class CreateArticle
+    {
+        public string? Title { get; set; }
+        public string? Picture { get; set; }
+        public string? PictureALT { get; set; }
+        public string? PictureTilte { get; set; }
+        public string? ShortDescription { get; set; }
+        public string? Description { get; set; }
+    }
+}
--- Blog/Pages/CreateArticle.cshtml.cs
+++ Blog/Pages/CreateArticle.cshtml.cs
@@ -1,12 +1,25 @@
-using Microsoft.AspNetCore.Mvc;
+﻿using Blog.Context;
+using Blog.Model.Entity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Blog.Pages
 {
-    public class CreateArticleModel : PageModel
+    public class CreateArticleModel(BlogContext _context) : PageModel
     {
+        public CreateArticle Command { get; set; }
+
         public void OnGet()
         {
+        }
+        public void OnPost(CreateArticle command)
+        {
+            var article = new Article(command.Title, command.Picture, command.PictureALT, command.PictureTilte, command.ShortDescription, command.Description);
+
+
+            _context.Add(article);
+            _context.SaveChanges();
+
+            ViewData["Success"] = "مقاله با موفقیت ذخیره شد";
         }
     }
 }
